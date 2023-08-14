5ec5c33c550fe1d81805e654f726906f0d8eb773
ahmed <asrawiahmed@gmail.com>
2023-08-14 09:20:11 -0700

    add script number : 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,13 +12,13 @@
  */
 int main(void)
 {
-int num;
+	int num;
 
 	for (num = 0; num <= 9; num++)
 	{
 		putchar((num % 10) + '0');
 		if (num == 9)
-		 continue;
+			continue;
 		
 		putchar(',');
 		putchar(' ');
@@ -26,5 +26,5 @@ int num;
 
 	putchar('\n');
 
-return (0);
+	return (0);
 }
