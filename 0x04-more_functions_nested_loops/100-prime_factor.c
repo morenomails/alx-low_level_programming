00326260f504a58b7c12e8126bb44f3e4ae97610
ahmed <asrawiahmed@gmail.com>
2023-08-17 17:28:23 -0700

    update task 11

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,26 +1,31 @@
 #include <stdio.h>
+
 /**
- * main - entry point
- *
- * Return: always 0 (sucess)
- */
+*main- prints the largest prime factor
+*of a number
+*
+*Return: returns 0
+*/
+
 int main(void)
 {
-	unsigned long int num = 612852475143;
-	unsigned long int prim;
+long number = 612852475143;
+int inc;
+while (inc++ < number / 2)
+{
+if (number % inc == 0)
+{
+number /= 2;
+continue;
+}
 
-	prim = 3;
-	while (prim < num / 2)
-	{
-		if ((num % prim) == 0)
-		{
-			if((prim % 3) == 2)
-				printf(",%lu ", prim);
-		}
+for (inc = 3; inc < number / 2; inc += 2)
+{
+if (number % inc == 0)
+number /= inc;
 
-		prim+=2;
-	}
-
-	putchar('\n');
-	return (0);
+}
+}
+printf("%ld\n", number);
+return (0);
 }
